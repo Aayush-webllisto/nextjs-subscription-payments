3715d261d2d1761dbc4c16aad35972a2a538b86d
Aayush Soni <95845559+Aayush-webllisto@users.noreply.github.com>
2021-12-27 18:05:21 +0530

    Update stripe-client.ts

--- utils/stripe-client.ts
+++ utils/stripe-client.ts
@@ -4,10 +4,13 @@ let stripePromise: Promise<Stripe | null>;
 
 export const getStripe = () => {
   if (!stripePromise) {
+    console.log('get stripe')
     stripePromise = loadStripe(
       process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY_LIVE ?? process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY ?? ''
     );
   }
+  
+  console.log(stripePromise)
 
   return stripePromise;
 };
